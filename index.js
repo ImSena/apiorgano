49a64694a89b3d07cf7407194471c3f64e4b956d
Bruno Sena <BrunoSena14@outlook.com>
2024-11-20 13:07:20 -0300

    Arrumado Cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import apiRoutes from './src/routes/apiRoutes.js'
+import cors from 'cors';
 
 dotenv.config();
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use('/api', apiRoutes)
+app.use('/api', apiRoutes);
 
 app.listen(PORT, ()=>{
     console.log('ta rodando');
